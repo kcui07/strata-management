15e5ab68519b2f5ef9876adbc7072899eda147df
Ke Cui <kcui0933@uni.sydney.edu.au>
2025-04-13 12:11:12 +1000

    fix: add header component to all pages

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,14 @@
-import Link from 'next/link';
-import styles from './Header.module.css';
+import Link from 'next/link'
+import styles from '@/styles/Header.module.css'
 
 export default function Header() {
   return (
     <nav className={styles.navbar}>
       <Link href="/">Home</Link>
-      <Link href="/owners">Owners Info</Link>
+      <Link href="/owners">Owners</Link>
       <Link href="/maintenance">Maintenance</Link>
       <Link href="/payments">Payments</Link>
       <Link href="/contact">Contact</Link>
     </nav>
-  );
+  )
 }
