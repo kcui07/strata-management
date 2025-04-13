dbf61ddabafa57ea3ea7976aca182b2ac937aa0c
Ke Cui <kcui0933@uni.sydney.edu.au>
2025-04-13 13:14:59 +1000

    update header with logo and style optimization

--- components/Header.js
+++ components/Header.js
@@ -5,12 +5,10 @@ import styles from '@/styles/Header.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
-      {/* 左边Logo */}
       <div className={styles.logo}>
         <Image src="/logo.png" alt="Logo" width={80} height={80} />
       </div>
 
-      {/* 右边导航栏 */}
       <nav className={styles.navbar}>
         <Link href="/">Home</Link>
         <Link href="/owners">Owners information</Link>
