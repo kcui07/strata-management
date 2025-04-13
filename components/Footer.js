36d5040340dfb52620903614b8de9431f050800d
Ke Cui <kcui0933@uni.sydney.edu.au>
2025-04-13 12:52:42 +1000

    feat: add footer and fix footer position

--- components/Footer.js
+++ components/Footer.js
@@ -1,23 +1,12 @@
+import styles from '@/styles/Footer.module.css';
+
 export default function Footer() {
-    return (
-      <footer style={{
-        backgroundColor: '#f77f00',
-        color: 'white',
-        padding: '20px 30px',
-        textAlign: 'left',
-        fontWeight: 'bold',
-      }}>
-        <h2 style={{
-          textTransform: 'uppercase',
-          fontSize: '24px',
-          marginBottom: '10px'
-        }}>
-          Contact Us
-        </h2>
-  
-        <p style={{ fontSize: '16px', margin: '5px 0' }}>Phone: +81 0403658976</p>
-        <p style={{ fontSize: '16px', margin: '5px 0' }}>Address: Level 7, XXX Building, NSW 2017, Australia</p>
-        <p style={{ fontSize: '16px', margin: '5px 0' }}>Email: storemanagement.2025@outlook.com</p>
-      </footer>
-    );
-  }
+  return (
+    <div className={styles.footer}>
+      <h2 style={{ fontSize: '20px' }}>CONTACT US</h2>
+      <p>Phone: +81 0403658976</p>
+      <p>Address: Level 7, XXX Building, NSW 2017, Australia</p>
+      <p>Email: storemanagement.2025@outlook.com</p>
+    </div>
+  )
+}
