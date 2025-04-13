88c516d6ac785a5ca42a64a55789b62c84c5cbac
Ke Cui <kcui0933@uni.sydney.edu.au>
2025-04-13 13:12:09 +1000

    add header logo & style update

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,23 @@
-import Link from 'next/link'
-import styles from '@/styles/Header.module.css'
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '@/styles/Header.module.css';
 
 export default function Header() {
   return (
-    <nav className={styles.navbar}>
-      <Link href="/">Home</Link>
-      <Link href="/owners">Owners information</Link>
-      <Link href="/maintenance">Maintenance</Link>
-      <Link href="/payments">Payments</Link>
-      <Link href="/contact">Contact Us</Link>
-    </nav>
-  )
+    <header className={styles.header}>
+      {/* 左边Logo */}
+      <div className={styles.logo}>
+        <Image src="/logo.png" alt="Logo" width={80} height={80} />
+      </div>
+
+      {/* 右边导航栏 */}
+      <nav className={styles.navbar}>
+        <Link href="/">Home</Link>
+        <Link href="/owners">Owners information</Link>
+        <Link href="/maintenance">Maintenance</Link>
+        <Link href="/payments">Payments</Link>
+        <Link href="/contact">Contact Us</Link>
+      </nav>
+    </header>
+  );
 }
